648ff76804f1e9f2a04d62d06382bbd4c72163dc
Juener <juener@gmail.com>
2023-04-25 16:56:30 +0100

    defining node version to vercel

--- src/components/structure/Error.jsx
+++ src/components/structure/Error.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function Error(props) {
     return (
         <Head>
-            <meta http-equiv="refresh" content="0; URL='/'" />
+            <meta httpEquiv="refresh" content="0; URL='/'" />
         </Head>
     )
 }
