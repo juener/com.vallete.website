0ec819e9471a5e02572e3324e7f6038046539cd1
Juener <juener@gmail.com>
2023-05-16 20:19:40 +0100

    removing materialize css

--- src/components/structure/Nav.jsx
+++ src/components/structure/Nav.jsx
@@ -1,5 +1,3 @@
-import Head from 'next/head';
-
 import Image from 'next/image';
 import valleteLogo from '/public/images/structure/logoJustDrawingWhite.png';
 
@@ -7,11 +5,8 @@ export default function Nav() {
 
     return (
         <>
-            <Head>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
-            </Head>
-            <div className="fixed-action-btn">
-                <a href="https://privileged.vallete.com" data-target="slide-out" className="sidenav-trigger show-on-large btn-floating btn-large pulse red darken-3"><i className="material-icons"><Image alt="backend" src={valleteLogo} /></i></a>
+            <div className='backend'>
+                <a href="https://privileged.vallete.com"><Image alt="backend" src={valleteLogo} /></a>
             </div>
         </>
     );
